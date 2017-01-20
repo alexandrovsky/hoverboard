2ed0e3893a3adc29ae13a186e024ecde15262b07
KyleGass <tim.moenkemoeller@web.de>
2017-01-20 20:24:24 +0100

    Asset Tunnel test

--- Assets/OSVRUnity/src/OsvrMouseLook.cs
+++ Assets/OSVRUnity/src/OsvrMouseLook.cs
@@ -1,104 +1,63 @@
-/// OSVR-Unity Connection
-///
-/// http://sensics.com/osvr
-///
-/// <copyright>
-/// Copyright 2015 Sensics, Inc.
-///
-/// Licensed under the Apache License, Version 2.0 (the "License");
-/// you may not use this file except in compliance with the License.
-/// You may obtain a copy of the License at
-///
-///     http://www.apache.org/licenses/LICENSE-2.0
-///
-/// Unless required by applicable law or agreed to in writing, software
-/// distributed under the License is distributed on an "AS IS" BASIS,
-/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-/// See the License for the specific language governing permissions and
-/// limitations under the License.
-/// </copyright>
-/// 
-/// This class is a modified version of MouseLook.cs distributed with Unity's Standard Assets package.
-/// The class name has been changed to avoid naming conflict and confusion.
-
 using UnityEngine;
 using System.Collections;
 
-namespace OSVR
-{
-    namespace Unity
-    {
-        // OsvrMouseLook rotates the transform based on the mouse delta.
-        // Minimum and Maximum values can be used to constrain the possible rotation
-        // Attach this script to an FPS style character.
-        public class OsvrMouseLook : MonoBehaviour
-        {
-            public KeyCode MouseLookToggleKey = KeyCode.M;
-            public bool useMouseLook = false;
-            public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2, RightJoystick = 3 }
-            public RotationAxes axes = RotationAxes.MouseXAndY;
-            public float sensitivityX = 15F;
-            public float sensitivityY = 15F;
+/// MouseLook rotates the transform based on the mouse delta.
+/// Minimum and Maximum values can be used to constrain the possible rotation
 
-            public float minimumX = -360F;
-            public float maximumX = 360F;
+/// To make an FPS style character:
+/// - Create a capsule.
+/// - Add the MouseLook script to the capsule.
+///   -> Set the mouse look to use LookX. (You want to only turn character but not tilt it)
+/// - Add FPSInputController script to the capsule
+///   -> A CharacterMotor and a CharacterController component will be automatically added.
 
-            public float minimumY = -60F;
-            public float maximumY = 60F;
+/// - Create a camera. Make the camera a child of the capsule. Reset it's transform.
+/// - Add a MouseLook script to the camera.
+///   -> Set the mouse look to use LookY. (You want the camera to tilt up and down like a head. The character already turns.)
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class MouseLook : MonoBehaviour {
 
-            float rotationY = 0F;
-            private float lastMouseLookTime = 0;
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
 
-            void Start()
-            {
-                // Make the rigid body not change rotation
-                if (GetComponent<Rigidbody>())
-                {
-                    GetComponent<Rigidbody>().freezeRotation = true;
-                }
-            }
+	public float minimumX = -360F;
+	public float maximumX = 360F;
 
-            void Update()
-            {
-                if (Input.GetKeyDown(MouseLookToggleKey) && Time.time - lastMouseLookTime > 0.3f)
-                {
-                    lastMouseLookTime = Time.time;
-                    useMouseLook = !useMouseLook;
-                }
-                if (!useMouseLook)
-                {
-                    return;
-                }
-                if (axes == RotationAxes.MouseXAndY)
-                {
-                    float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
 
-                    rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-                    rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+	float rotationY = 0F;
 
-                    transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-                }
-                else if (axes == RotationAxes.RightJoystick)
-                {
-                    if (Mathf.Abs(Input.GetAxis("Right Joystick")) > 0.08f)
-                    {
-                        transform.Rotate(0, Input.GetAxis("Right Joystick") * sensitivityX, 0);
-                    }
-                }
-                else if (axes == RotationAxes.MouseY)
-                {
-                    rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-                    rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-
-                    transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-                }
-                else if (axes == RotationAxes.MouseX)
-                {
-                    float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-
-                    transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-                }
-            }
-        }
-    }
+	void Update ()
+	{
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+			
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+		}
+	}
+	
+	void Start ()
+	{
+		// Make the rigid body not change rotation
+		if (rigidbody)
+			rigidbody.freezeRotation = true;
+	}
 }

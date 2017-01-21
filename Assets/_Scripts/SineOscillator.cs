f073c4b4eafa10cae9e3dbba7e5c690f89040147
Paul Koch <kulpae@uraniumlane.net>
2017-01-21 15:39:41 +0100

    Main Menu + BG Music + skybox

---
+++ Assets/_Scripts/SineOscillator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineOscillator : MonoBehaviour {
+
+	public float amplitudeX = 10;
+	public float amplitudeY = 0;
+	public float speed = 1f;
+
+	private RectTransform rect;
+	private Vector2 startPos;
+	private Vector2 endPos;
+
+	// Use this for initialization
+	void Start () {
+		rect = gameObject.GetComponent<RectTransform> ();
+		startPos = rect.anchoredPosition;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		rect.anchoredPosition = new Vector2(
+			startPos.x + Mathf.Sin(Time.time * speed) * amplitudeX, 
+			startPos.y + Mathf.Sin(Time.time * speed) * amplitudeY
+		);
+	}
+}

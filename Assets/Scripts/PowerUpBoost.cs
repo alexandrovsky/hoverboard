2087aefe8e52dd12baf057610a90daba433a91f8
dmitry alexandrovsky <alexandrovsky.dmitry@gmail.com>
2017-01-22 11:10:53 +0100

    i forgot

---
+++ Assets/Scripts/PowerUpBoost.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpBoost : PowerUp {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override IEnumerator Callback(Player player){
+		player.powerUps.Add(PowerUpType.Speed);
+		float old = player.velocity;
+		player.velocity += value;
+		yield return new WaitForSeconds(cooldown);
+		int i = -1;
+		for(i = 0; i < player.powerUps.Count; i++){
+			if(player.powerUps[i] == PowerUpType.Speed){
+				break;
+			}
+		}
+		if(i >= 0){
+			player.powerUps.RemoveAt(i);
+		}
+		player.velocity = old;
+		Debug.Log("power up deactivate");
+	}
+}

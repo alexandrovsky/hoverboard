0e295cf96d7bf38b703ec3782ed71b854cf589c0
dmitry alexandrovsky <alexandrovsky.dmitry@gmail.com>
2017-01-22 15:04:17 +0100

    HUD in OnGUI

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -4,9 +4,36 @@ using UnityEngine.UI;
 public class HUD : MonoBehaviour {
 
 	public Text velocityLabel, scoreLabel, timeLabel;
+	float velocity, score, time;
+	void OnGUI(){
+
+		Rect left = new Rect(0, 0, Screen.width/2, 50);
+		Rect right = new Rect(Screen.width/2, 0, Screen.width/2, 50);
+
+		GUILayout.BeginArea(left);
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Speed: " +  ((int)(velocity * 10f)).ToString());
+		GUILayout.Label(time + "s");
+		GUILayout.Label("Score: " + score);
+		GUILayout.EndHorizontal();
+		GUILayout.EndArea();
+
+		GUILayout.BeginArea(right);
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Speed: " +  ((int)(velocity * 10f)).ToString());
+		GUILayout.Label(time + "s");
+		GUILayout.Label("Score: " + score);
+		GUILayout.EndHorizontal();
+		GUILayout.EndArea();
+		
+	}
+
 
 	public void SetValues (int score, int time, float velocity) {
-		
+		this.score = score;
+		this.time = time;
+		this.velocity = velocity;
+
 		velocityLabel.text = "Speed: " +  ((int)(velocity * 10f)).ToString();
 		scoreLabel.text = "Points: " +  score.ToString();
 		timeLabel.text = time.ToString() + "s";
